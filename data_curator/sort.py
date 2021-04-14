b1b64ba4a23526c880ecb47828b4527845dceb6e
Emily Gott <gott@scc4.bu.edu>
2021-04-13 22:45:26 -0400

    version with one arg only

--- data_curator/sort.py
+++ data_curator/sort.py
@@ -10,14 +10,10 @@ except IndexError as ie:
     print("Call sort.py file_to_open name_to_write(optional)\n")
 
 d ={} #dictionary for counts (barcode:count)
-i=0 #for debugging fd
 if df:
     for line in df:
-        i+=1#fd
-        if i > 100: #fd
-            break #fd
         #if line has barcode, continue. Else skip
-        elif line.startswith(b'@'): #after debugging, make this if
+        if line.startswith(b'@'): #after debugging, make this if
             bc = line.decode("utf-8").split(' ')
             #Only use lines w/barcodes
             if len(bc) > 2:
@@ -29,29 +25,28 @@ if df:
                 else:
                     d[code]+=1 
 
-#will write to csv 
-y = False
-#Checks if a file name was specified
-if sys.argv[2]:
-    f = sys.argv[2]
-else: #if not, use original file name
-    y = True
-    f = sys.argv[1]
+    #will write to csv 
+#    y = False
+    #Checks if a file name was specified
+#    if sys.argv[2]:
+#        f = sys.argv[2]
+ #   else: #if not, use original file name
+#        y = True
+f = sys.argv[1]
     #if file path, delete anything but the actual name
     #Only if you use sys.argv[1]
-    if "/" in f:
-        f = f.split("/")
-        f = f[len(f)-1]
-        f = ''.join(f)
+if "/" in f:
+    f = f.split("/")
+    f = f[len(f)-1] #last one is file name
+    f = ''.join(f)
 #Formats the output into a csv no matter what
 f = f.split(".") 
 #if there's a .{anything} will remove
 if len(f) > 1:
     f = f[0]
 f = ''.join(f)
-if y == True: #if we used the argv[1] argument, add "_output"
-    f += "_output"
-f += ".csv" #ensures that a .csv
+#    if y == True: #if we used the argv[1] argument, add "_output"
+f += "_output.csv" #ensures that a .csv
 
 with open(f, "w") as file:
     w = csv.writer(file, delimiter=',')
