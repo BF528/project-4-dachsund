af019dba81489b49289115627f945af0a0e74892
allison-nau <anau@bu.edu>
2021-04-18 22:45:57 -0400

    Update umap_analyst.R
    Still working with example clusters. Figures saved into file

--- analyst/umap_analyst.R
+++ analyst/umap_analyst.R
@@ -1,84 +1,82 @@
-# Load pacakges:
+# Load packages:
 library(Seurat)
 # Say yes to installing miniconda
 library(dplyr)
 
 prog_data <- readRDS("GSM2230760_seurat_anau.rda")
-prog_data2 <- readRDS("GSM2230760_seurat_anau.rda")
 
 # Finding differentially expressed features (cluster biomarkers)
 # Find markers for every cluster compared to all remaining cells, report 
 # only the positive ones
-# TODO: minimum percent 0.25?? logfc.threshold
+# Methods available to use: https://satijalab.org/seurat/archive/v3.0/de_vignette.html
+# Default: Wilcox
+# "LR": logistic regression
+# Minimum percent: detected in a minimum proportion of cells in either of the two pops
+# logfc.threshold
 diff_markers <- FindAllMarkers(prog_data, only.pos=TRUE, min.pct=0.25, 
                                logfc.threshold=0.25)
+# Minimum percent 0.8 came back with nothing, try 0.5
+diff_markers_high_percent <- FindAllMarkers(prog_data, only.pos=TRUE, min.pct=0.5, 
+                               logfc.threshold=0.25)
+
+diff_markers_sign <- diff_markers[diff_markers$p_val_adj<0.05,]
+
+# Save to csv file:
+write.csv(diff_markers, file="diff_genes.csv")
+write.csv(diff_markers_sign, file="diff_genes_sign.csv")
 
 # Look at only the top ones:
 top5 <- diff_markers %>% group_by(cluster) %>% top_n(n=5, wt=avg_log2FC)
 top3 <- diff_markers %>% group_by(cluster) %>% top_n(n=3, wt=avg_log2FC)
 top10 <- diff_markers %>% group_by(cluster) %>% top_n(n=10, wt=avg_log2FC)
+top1 <- diff_markers %>% group_by(cluster) %>% top_n(n=1, wt=avg_log2FC)
 
-VlnPlot(prog_data, "CD68", pt.size = 0.0001)
+# Save a list of genes:
+top1genes <- top1$gene
+top1genes
 
-#TODO: Look at VLN plots??
-# TODO: MAKE THE IMAGES MUCH MUCH SMALLER!!!!
-for (feature in c("GCG", "INS",  "SST", "PPY", "GHRL", "KRT19",
-                  "CPA1", "PDGFRB", "VWF", "PECAM1", "CD34",
-                  "CD163", "CD68", 
-                  "TPSAB1", "KIT", "CPA3")){
-  #TODO temp_file <- paste(feature, ".png", sep="")
-  #TODO png(temp_file, width=800, height=800)
-  # temp_vln <- VlnPlot(prog_data, features=c(feature), pt.size=0.0001)
-  VlnPlot(prog_data, features=c(feature), pt.size=0.0001)
-  # dev.copy(png, paste(feature, ".png", sep="))
-  # dev.off()
-  # TODO fix the paste
-  #TODO save.image(temp_vln, file=temp_file)
-}
-
-
-# TODO: try to make plots smaller, including maybe removing plotting the individual points
-
+# Make violin plots for genes or interest:
 # myvln <- VlnPlot(prog_data, features=c("GCG", "INS",  "SST", "PPY", "GHRL", "KRT19",
 #                                       "CPA1", "PDGFRB", "VWF", "PECAM1", "CD34",
 #                                       "CD163", "CD68", "IgG", "CD3", "CD8",
 #                                       "TPSAB1", "KIT", "CPA3"), pt.size=0.0001)
 
 # TODO: add back in "IgG", "CD3", "CD8",
-VlnPlot(prog_data, features=c("GCG", "INS",  "SST", "PPY", "GHRL", "KRT19",
-                                       "CPA1", "PDGFRB", "VWF", "PECAM1", "CD34",
-                                       "CD163", "CD68", 
-                                       "TPSAB1", "KIT", "CPA3"), pt.size=0,
-        combine=TRUE)
 
+# Make violin plot of the top expressed gene of each cluster:
+vplot1 <- VlnPlot(prog_data, features=top1genes, pt.size=0, combine=TRUE)
+vplot1
 
-#TODO: fix saving 
-save.image("myvln.png")
-save.image("myvln.jpg")
+#Save violin plot of top genes:
+png(filename="vplotTop1.png", width=1200, height=800)
+vplot1
+dev.off()
 
-
-# TODO: try original object
-
-# Try heatmap:
+# Make heatmaps:
 DoHeatmap(prog_data, features=top3$gene)
 DoHeatmap(prog_data, features=top5$gene)
 DoHeatmap(prog_data, features=top10$gene)
-
-DimHeatmap(prog_data, dims=1, cells=500, balanced=TRUE)
-
-map <- DoHeatmap(prog_data, features=top10$gene)
+map <- DoHeatmap(prog_data, features=top5$gene)
 map
-save.image("map.png")
-#TODO: delete? DoHeatmap(diff_markers, features=top10$gene) + NoLegend()
+
+# Save heatmap:
+png(filename="heatmapTop5.png", width=1200, height=800)
+map
+dev.off()
 
 
-#TODO: fix all images
+#TODO: UMAP cluster names
 
 # Run UMAP:
 #TODO: why dims 1:10
 my_umap1 <- RunUMAP(prog_data, dims=1:10)
-#TODO my_umap2 <- RunUMAP(prog_data, dims=1:20)
+my_umap1
 
 # Plot UMAP:
-DimPlot(my_umap1, reduction="umap", label=TRUE)
-#TODO DimPlot(my_umap, reduction="umap2")
+umap_plot <- DimPlot(my_umap1, reduction="umap", label=TRUE)
+umap_plot
+
+# Save UMAP:
+png(filename="umap.png")
+umap_plot
+dev.off()
