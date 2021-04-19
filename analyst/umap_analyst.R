113464c49b9d663cc689b75108e67735f0750158
allison-nau <anau@bu.edu>
2021-04-19 19:16:11 -0400

    Update umap_analyst.R

--- analyst/umap_analyst.R
+++ analyst/umap_analyst.R
@@ -5,6 +5,16 @@ library(dplyr)
 
 prog_data <- readRDS("GSM2230760_seurat_anau.rda")
 
+# Label cluster identities (will vary for each study):
+# TODO this rename cluster isn't working
+new_ids <- c("Fake Alpha", "Fake Beta", "Fake Delta", "Fake Gamma",
+             "Fake Epsilon", "Fake Ductal", "Fake Acinar", "Fake Stellate",
+             "Fake Vascular", "Fake Macrophage", "Fake Cytotoxic T", 
+             "Fake Mast", "Fake 13")
+
+names(new_ids) <- levels(prog_data)
+prog_data2 <- RenameIdents(prog_data, new_ids)
+
 # Finding differentially expressed features (cluster biomarkers)
 # Find markers for every cluster compared to all remaining cells, report 
 # only the positive ones
@@ -13,10 +23,10 @@ prog_data <- readRDS("GSM2230760_seurat_anau.rda")
 # "LR": logistic regression
 # Minimum percent: detected in a minimum proportion of cells in either of the two pops
 # logfc.threshold
-diff_markers <- FindAllMarkers(prog_data, only.pos=TRUE, min.pct=0.25, 
+diff_markers <- FindAllMarkers(prog_data2, only.pos=TRUE, min.pct=0.25, 
                                logfc.threshold=0.25)
 # Minimum percent 0.8 came back with nothing, try 0.5
-diff_markers_high_percent <- FindAllMarkers(prog_data, only.pos=TRUE, min.pct=0.5, 
+diff_markers_high_percent <- FindAllMarkers(prog_data2, only.pos=TRUE, min.pct=0.5, 
                                logfc.threshold=0.25)
 
 diff_markers_sign <- diff_markers[diff_markers$p_val_adj<0.05,]
@@ -44,7 +54,7 @@ top1genes
 # TODO: add back in "IgG", "CD3", "CD8",
 
 # Make violin plot of the top expressed gene of each cluster:
-vplot1 <- VlnPlot(prog_data, features=top1genes, pt.size=0, combine=TRUE)
+vplot1 <- VlnPlot(prog_data2, features=top1genes, pt.size=0, combine=TRUE)
 vplot1
 
 #Save violin plot of top genes:
@@ -53,10 +63,11 @@ vplot1
 dev.off()
 
 # Make heatmaps:
-DoHeatmap(prog_data, features=top3$gene)
-DoHeatmap(prog_data, features=top5$gene)
-DoHeatmap(prog_data, features=top10$gene)
-map <- DoHeatmap(prog_data, features=top5$gene)
+# TODO: add dendograms?
+DoHeatmap(prog_data2, features=top3$gene)
+DoHeatmap(prog_data2, features=top5$gene)
+DoHeatmap(prog_data2, features=top10$gene)
+map <- DoHeatmap(prog_data2, features=top5$gene)
 map
 
 # Save heatmap:
@@ -69,7 +80,7 @@ dev.off()
 
 # Run UMAP:
 #TODO: why dims 1:10
-my_umap1 <- RunUMAP(prog_data, dims=1:10)
+my_umap1 <- RunUMAP(prog_data2, dims=1:10)
 my_umap1
 
 # Plot UMAP:
